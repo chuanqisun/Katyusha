ae3891adef5b7749d4fc3a41d2442a1f9d8cc88d
Chuanqi <henrysun918@gmail.com>
2019-06-11 22:08:52 -0700

    move svg to html

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,10 @@
 <script>
   import MenuBar from "./components/MenuBar.svelte";
-  import SvgIcons from "./components/SvgIcons.svelte";
 </script>
 
 <style>
 
 </style>
 
-<SvgIcons />
 <MenuBar />
 <main>Hello svelte</main>
--- src/components/SvgIcons.svelte
+++
@@ -1,8 +0,0 @@
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
-  <symbol id="svg-minimize" viewBox="0 0 16 16">
-    <line x1="3" y1="8.5" x2="13" y2="8.5" stroke="currentColor" />
-  </symbol>
-  <symbol id="svg-close" viewBox="0 0 16 16">
-    <path d="M3.5 12.5L12.5 3.5M12.5 12.5L3.5 3.5" stroke="currentColor" />
-  </symbol>
-</svg>
