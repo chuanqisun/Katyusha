70de11ad61ccc78a45665e09c51054b67fa49074
Chuanqi <henrysun918@gmail.com>
2019-06-11 22:02:02 -0700

    implement menu bar

--- src/components/MenuBar.svelte
+++ src/components/MenuBar.svelte
@@ -3,25 +3,62 @@
 </script>
 
 <style>
+  :root {
+    --menu-bar-height: 1.875rem;
+    --menu-bar-background-color: #323130;
+    --menu-bar-background-color-hover: #505050;
+    --menu-bar-text-color: #cccccc;
+    --menu-bar-text-color-strong: white;
+    --window-action-width: 2.875rem;
+    --window-action-destructive-background: #d0062a;
+  }
 
+  .app-menu {
+    height: var(--menu-bar-height);
+
+    -webkit-app-region: drag;
+    display: flex;
+    justify-content: space-between;
+    background-color: var(--menu-bar-background-color);
+    color: var(--menu-bar-text-color);
+  }
+
+  .app-menu__window-actions {
+    display: flex; /* prevent button-to-button gaps*/
+  }
+
+  .app-menu__window-action {
+    color: var(--menu-bar-text-color);
+    background-color: var(--menu-bar-background-color);
+    border: none;
+    width: var(--window-action-width);
+    height: 100%;
+  }
+
+  .app-menu__window-action:hover {
+    color: var(--menu-bar-text-color-strong);
+    background-color: var(--menu-bar-background-color-hover);
+  }
+
+  .app-menu__window-action--destructive:hover {
+    background-color: var(--window-action-destructive-background);
+  }
 </style>
 
 <header class="app-menu">
-  <div class="app-menu__title">
-    <button>Katyusha</button>
-  </div>
+  <div class="app-menu__title">Katyusha</div>
   <div class="app-menu__window-actions">
     <button
-      class="app-menu__window-action-button"
+      class="app-menu__window-action"
       on:click={() => ipcRenderer.send('tryMinimize')}>
-      <svg width="0.6rem" height="0.6rem">
+      <svg class="icon">
         <use xlink:href="#svg-minimize" />
       </svg>
     </button>
     <button
-      class="app-menu__window-action-button"
+      class="app-menu__window-action app-menu__window-action--destructive"
       on:click={() => ipcRenderer.send('tryClose')}>
-      <svg width="0.6rem" height="0.6rem">
+      <svg class="icon">
         <use xlink:href="#svg-close" />
       </svg>
     </button>
--- src/components/SvgIcons.svelte
+++ src/components/SvgIcons.svelte
@@ -1,8 +1,8 @@
 <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
   <symbol id="svg-minimize" viewBox="0 0 16 16">
-    <text x="0" y="15" fill="currentColor">-</text>
+    <line x1="3" y1="8.5" x2="13" y2="8.5" stroke="currentColor" />
   </symbol>
   <symbol id="svg-close" viewBox="0 0 16 16">
-    <text x="0" y="15" fill="currentColor">x</text>
+    <path d="M3.5 12.5L12.5 3.5M12.5 12.5L3.5 3.5" stroke="currentColor" />
   </symbol>
 </svg>
