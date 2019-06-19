126fd6dc446e5fc3f774ea9ec7b69bdf5e48cba8
Chuanqi <henrysun918@gmail.com>
2019-06-18 22:02:39 -0700

    styling settings form

--- src/components/AppSettingsForm.svelte
+++ src/components/AppSettingsForm.svelte
@@ -15,12 +15,32 @@
 </script>
 
 <style>
-
+  .settings-items {
+    display: grid;
+    gap: 1rem;
+  }
 </style>
 
 <form class="form" on:submit={onSubmit}>
-  <div class="form__field">
-    <button on:click={onImport}>Import environments</button>
-    <button on:click={onExport}>Export environments</button>
+  <h3>Environments</h3>
+  <div class="settings-items">
+    <button
+      on:click={onImport}
+      class="btn btn--icon-text btn--secondary"
+      type="button">
+      <svg class="btn__icon">
+        <use xlink:href="#svg-plus" />
+      </svg>
+      <span class="btn__text">Import from file</span>
+    </button>
+    <button
+      on:click={onExport}
+      class="btn btn--icon-text btn--secondary"
+      type="button">
+      <svg class="btn__icon">
+        <use xlink:href="#svg-download" />
+      </svg>
+      <span class="btn__text">Export to file</span>
+    </button>
   </div>
 </form>
