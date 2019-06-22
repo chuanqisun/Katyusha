2ca0d2c87a076108bdb1018f44ae265c4e03b210
Chuanqi <henrysun918@gmail.com>
2019-06-22 15:39:19 -0700

    support manual launch

--- src/helpers/launch.js
+++ src/helpers/launch.js
@@ -2,13 +2,24 @@ require('chromedriver');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const ChromeBuilder = new Builder().forBrowser('chrome');
 
-export async function launch({ url, username, password }) {
+export async function launch({ url, auth, ...rest }) {
   let driver = await ChromeBuilder.build();
   await driver.get(url);
-  await driver.wait(until.elementLocated(By.name('loginfmt')));
-  await driver.findElement(By.name('loginfmt')).sendKeys(username, Key.RETURN);
-  await driver.wait(until.elementLocated(By.id('displayName')));
-  await driver.findElement(By.name('passwd')).sendKeys(password, Key.RETURN);
-  await driver.wait(until.elementLocated(By.id('KmsiCheckboxField')));
-  await driver.findElement(By.id('idSIButton9')).click();
+
+  switch (auth) {
+    case 'aad-basic':
+      console.log('[launch] aad-basic');
+      const { username, password } = rest;
+      await driver.wait(until.elementLocated(By.name('loginfmt')));
+      await driver.findElement(By.name('loginfmt')).sendKeys(username, Key.RETURN);
+      await driver.wait(until.elementLocated(By.id('displayName')));
+      await driver.findElement(By.name('passwd')).sendKeys(password, Key.RETURN);
+      await driver.wait(until.elementLocated(By.id('KmsiCheckboxField')));
+      await driver.findElement(By.id('idSIButton9')).click();
+      break;
+    case 'manual':
+    default:
+      console.log('[launch] manual');
+      break;
+  }
 }
