ef51afcbff5470576e3ddcd5e4ac8e15b3849c6c
Chuanqi <henrysun918@gmail.com>
2019-06-22 13:47:54 -0700

    clean up external dependencies

--- renderer.js
+++
@@ -1,5 +0,0 @@
-const {ipcRenderer} = require('electron')
-
-// Handle DOM events
-minimizeButton.onclick = () => ipcRenderer.send('tryMinimize');
-closeButton.onclick = () => ipcRenderer.send('tryClose');
